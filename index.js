1429afc2c127ccedb5405a1e007d428b53faf331
ouichen ayoub <ayoub.wichen705@gmail.com>
2024-02-01 12:36:01 +0100

    update-x

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const express = require('express');
 var cors = require('cors');
 const app = express();
-const fetchDataRoute = require('./fetchData');
+const fetchDataRoute = require('./src/fechData');
 
 app.use('/fetchData', fetchDataRoute);
 
