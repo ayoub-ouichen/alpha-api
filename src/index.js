e1350578e15ac80521879fa618336199904d28a6
ouichen ayoub <ayoub.wichen705@gmail.com>
2024-02-02 17:47:32 +0100

    Google Sheets 2

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-var cors = require('cors');
-const app = express();
-const fetchDataRoute = require('./fechData');
-
-app.use('/fetchData', fetchDataRoute);
-
-module.exports = app;
--- src/server.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const app = require('./index');
-const server = http.createServer(app);
-const port = 3000;
-server.listen(port);
